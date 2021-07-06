c962d8e574160c48f1767b0624d37e95937b1522
minzzan <69343830+minzzan@users.noreply.github.com>
2021-07-06 15:31:04 +0900

    Update realTime_eyeTracking3.py

--- realTime_eyeTracking/realTime_eyeTracking3.py
+++ realTime_eyeTracking/realTime_eyeTracking3.py
@@ -17,11 +17,6 @@ import threading
 import sqlite3
 from sqlite3.dbapi2 import Date
 import datetime
-###################flask 관련####################
-import json
-import sys
-from random import random
-from flask import Flask, render_template, make_response, redirect, url_for
 
 
 coord=np.ndarray([])
@@ -229,56 +224,3 @@ while(True):
     
 cap.release()
 cv2.destroyAllWindows()
-
-###############################################################################################
-
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html', data='test')
-
-def live_data(concen_v,concen_d):
-    # Create a PHP array and echo it as JSON
-    data = [concen_d,concen_v]
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-    return response
-
-def live_data_pose(concen_v,concen_d):
-    # Create a PHP array and echo it as JSON
-    data = [concen_d,concen_v]
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-    return response
-
-
-@app.route('/live-data')
-def db_query():
-    db = sqlite3.connect("DB2.db")
-    #db.row_factory = sqlite3.Row
-    query = db.execute("SELECT value,datetime FROM concentration ORDER BY datetime DESC LIMIT 1").fetchall()
-    db.close()
-
-    for q in query:
-        concen_v=q[0]
-        concen_d=q[1]
-    resopnse=live_data(concen_v,concen_d)
-    return resopnse
-
-@app.route('/live-data-pose')
-def db_pose():
-    db = sqlite3.connect("DB2.db")
-    query_pose=db.execute("SELECT posture,datetime FROM pose ORDER BY datetime DESC LIMIT 1").fetchall()
-
-    for q in query_pose:
-        pose_v=q[0]
-        pose_d=q[1] 
-    resopnse=live_data_pose(pose_v,pose_d)
-    return resopnse
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
